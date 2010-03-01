c7fd6a49fcab79bc1af9f3275ab4f01837059221
Casey Deen <2marksplash@gmail.com>
2010-03-01 17:10:47 -0600

    Added a few more function headers to evaluate.py

--- hitchhiker/evaluate.py
+++ hitchhiker/evaluate.py
@@ -1,6 +1,10 @@
 from hitchiker.gameplay import Player
 
-def control_Probability(hand, suit):
+def controlProbability(hand, suit):
     """Returns the probability that the given hand controls the given suit"""
     
-    
+def calculateLeadProbability(hand, suit):
+    """Returns a list corresponding to the probabilities that each domino in HAND will be able to used as a winning lead, assuming SUIT is trump"""
+
+def calculateOffProbability(hand, suit):
+    """Returns a list corresponding to the probabilities that each domino in HAND will be vulnerable as a off.  Probably should factor in the 'dangerousness' of each suit.  (i.e. ThreeOne is a less dangerous off than FiveFour)"""
